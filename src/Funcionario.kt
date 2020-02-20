1079fc4437e9141d5926eab981b5b2c3e9044b5e
Alex Felipe <alexfelipevieira@gmail.com>
2020-02-20 10:06:46 -0300

    1.4

--- src/Funcionario.kt
+++ src/Funcionario.kt
@@ -1,9 +1,26 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double
+    val salario: Double,
+    val tipo: Int //0 funcionario, 1 gerente, 2 diretor
 ){
     fun bonificacao(): Double {
-        return salario * 0.1
+        when (tipo) {
+            0 -> {
+                return salario * 0.1
+            }
+            1 -> {
+                return salario * 0.2
+            }
+            else -> {
+                return salario * 0.3
+            }
+        }
+    }
+
+    fun autentica(senha: Int){
+        if(tipo == 1){
+            // pode autenticar
+        }
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -4,7 +4,8 @@ fun main() {
     val alex = Funcionario(
         nome = "Alex",
         cpf = "111.111.111-11",
-        salario = 1000.0
+        salario = 1000.0,
+        tipo = 0
     )
 
     println("nome ${alex.nome}")
