c66b15081ea37446ad36561fc7b53f8192e9a11f
Alex Felipe <alexfelipevieira@gmail.com>
2020-02-20 09:46:57 -0300

    1.3

---
+++ src/Funcionario.kt
@@ -0,0 +1,9 @@
+class Funcionario(
+    val nome: String,
+    val cpf: String,
+    val salario: Double
+){
+    fun bonificacao(): Double {
+        return salario * 0.1
+    }
+}
--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,16 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    testaComportamentosConta()
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
+
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salario ${alex.salario}")
+    println("bonificação ${alex.bonificacao()}")
 }
 
 
