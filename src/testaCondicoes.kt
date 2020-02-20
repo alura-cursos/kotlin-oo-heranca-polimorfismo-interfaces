cb51d7cc3d5fdc55ee36a011e6896e38b27a1266
Alex Felipe <alexfelipevieira@gmail.com>
2020-02-20 09:18:48 -0300

    1.2

---
+++ src/testaCondicoes.kt
@@ -0,0 +1,15 @@
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
+}
